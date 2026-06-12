083dbafd0eef655cac2053fcbde6ddbac06cda93
rozer <shubhamsinha07084@gmail.com>
2026-06-12 20:29:16 +0530

    updated models

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,32 +1,27 @@
 import re
 import nltk
-
 from nltk.corpus import stopwords
 
 
 def download_nltk_resources():
     """
-    Download required NLTK resources.
-    Streamlit Cloud safe version.
+    Download stopwords if not available.
     """
 
     try:
         nltk.data.find("corpora/stopwords")
-
     except LookupError:
         nltk.download("stopwords")
 
 
 download_nltk_resources()
 
-STOP_WORDS = set(
-    stopwords.words("english")
-)
+STOP_WORDS = set(stopwords.words("english"))
 
 
 def clean_text(text):
     """
-    Basic text cleaning
+    Clean text.
     """
 
     text = str(text).lower()
@@ -48,25 +43,24 @@ def clean_text(text):
 
 def remove_stopwords(text):
     """
-    Remove stop words
+    Remove stopwords using split().
+    No word_tokenize().
     """
 
     tokens = text.split()
 
     filtered_tokens = [
-        word
-        for word in tokens
-        if word not in STOP_WORDS
+        token
+        for token in tokens
+        if token not in STOP_WORDS
     ]
 
-    return " ".join(
-        filtered_tokens
-    )
+    return " ".join(filtered_tokens)
 
 
 def preprocess_text(text):
     """
-    Complete preprocessing pipeline
+    Complete preprocessing pipeline.
     """
 
     text = clean_text(text)
