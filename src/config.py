6e982d79cbba78472e8eb47409975005ccbe92dd
rozer <shubhamsinha07084@gmail.com>
2026-06-12 19:38:13 +0530

    Added configuration code

---
+++ src/config.py
@@ -0,0 +1,15 @@
+MODEL_PATH = "models/emotion_model.pkl"
+
+VECTORIZER_PATH = "models/tfidf_vectorizer.pkl"
+
+TRAIN_DATA_PATH = "data/train.txt"
+
+MAX_FEATURES = 5000
+
+TEST_SIZE = 0.2
+
+RANDOM_STATE = 42
+
+MODEL_NAME = "Linear SVC"
+
+MODEL_ACCURACY = "88.81%"
